2a1c59fb9cae0066333a23b59d0944aa28da6c1e
20nschmidt <20nschmidt@WA-248-9590.schools.westford-ma.gov>
2019-12-20 08:26:37 -0500

    12/20/2019 school

--- studentDatabase/src/database/addDlg.java
+++ studentDatabase/src/database/addDlg.java
@@ -1,23 +1,101 @@
 package database;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
 import BreezySwing.*;
+import java.awt.event.*;
 
 public class addDlg extends GBDialog {
 	
-	JComboBox level = addComboBox(1,1,1,1);
+	ButtonGroup schoolLevel = new ButtonGroup();
+    JRadioButton person  = addRadioButton ("Person", 1,1,1,1);
+    JRadioButton student  = addRadioButton ("Student", 1,2,1,1);
+    JRadioButton underGrad   = addRadioButton ("Under Graduate", 1,3,1,1); 
+    JRadioButton grad = addRadioButton ("Graduate", 1,4,1,1);
 	
+    JLabel nameLabel = addLabel("Name:",2,1,1,1);
+    JTextField name = addTextField("",2,2,2,1);
+    
+    JLabel stuIdLabel = addLabel("Student ID:",3,1,1,1);
+    JTextField studentID = addTextField("",3,2,2,1);
+    
+    JComboBox grade = addComboBox(4,2,1,1);
+    
+    JLabel majorLabel = addLabel("Major:",4,1,1,1);
+    JTextField major = addTextField("",4,2,2,1);
+   
+    
+    
+    private ChangeListener cl = new ChangeListener() {
+    	@Override
+    	public void stateChanged(ChangeEvent e) {
+    		JRadioButton source = (JRadioButton) e.getSource();
+    		
+    		if(source==person && person.isSelected()) {
+				stuIdLabel.setVisible(false);
+				studentID.setVisible(false);
+				
+				grade.setVisible(false);
+				
+				majorLabel.setVisible(false);
+				major.setVisible(false);
+			}
+			if(source==student && student.isSelected()) {
+				stuIdLabel.setVisible(true);
+				studentID.setVisible(true);
+				
+				grade.setVisible(false);
+				
+				majorLabel.setVisible(false);
+				major.setVisible(false);
+			}
+			if(source==underGrad && underGrad.isSelected()) {
+				stuIdLabel.setVisible(true);
+				studentID.setVisible(true);
+				
+				grade.setVisible(true);
+				
+				majorLabel.setVisible(false);
+				major.setVisible(false);
+			}
+			if(source==grad && grad.isSelected()) {
+				stuIdLabel.setVisible(true);
+				studentID.setVisible(true);
+				
+				grade.setVisible(false);
+				
+				majorLabel.setVisible(true);
+				major.setVisible(true);
+			}
+    	}
+    };
+    
+    
 	JFrame parentClass;
 	
 	public addDlg(JFrame parent,int type) {
 		super(parent);
-		level.addItem("Undergraduate");
-		level.addItem("Graduate");
 		setTitle(format(type));
+		schoolLevel.add(person);
+		schoolLevel.add(student);
+		schoolLevel.add(underGrad);
+		schoolLevel.add(grad);
+		person.addChangeListener(cl);
+		student.addChangeListener(cl);
+		underGrad.addChangeListener(cl);
+		grad.addChangeListener(cl);
+		grade.addItem("Freshman");
+		grade.addItem("Sophmore");
+		grade.addItem("Junior");
+		grade.addItem("Senior");
+		person.setSelected(true);
 		setDlgCloseIndicator("Cancel");
 		setSize(400, 200);
 		setLocationRelativeTo(null);
 		parentClass=parent;
+		
 	}
 	
 	public void buttonClicked(JButton buttonObj) {
@@ -26,11 +104,9 @@ public class addDlg extends GBDialog {
 	
 	private String format(int type) {
 		if(type==1) {
-			level.setVisible(false);
 			return"Add Undergrad";
 		}
 		if(type==2) {
-			level.setVisible(false);
 			return"Add Grad";
 		}
 		return "Add";
